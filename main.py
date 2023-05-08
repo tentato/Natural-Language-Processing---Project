49e152dc78d6dd1d1b59671d0acccd775cf07af2
tentato <alepaw99@gmail.com>
2023-05-08 22:47:05 +0200

    ensambles chosen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from nltk.corpus import stopwords
 
 input_path = 'dataset/spam_ham_dataset.csv'
