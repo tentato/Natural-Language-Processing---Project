22854a0806f29fea7dbd6672bacae28d044e3e21
tentato <alepaw99@gmail.com>
2023-05-08 23:11:14 +0200

    variables renamed

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ from sklearn.ensemble import GradientBoostingClassifier
 from nltk.corpus import stopwords
 import re
 
-input_path = 'dataset/spam_ham_dataset.csv'
-
-dataset = pd.read_csv(input_path)
-X = dataset['text']
-y = dataset['label_num']
-
-words_df = [preprocessing(text) for text in X]
-
-print(words_df[0])
-
 def preprocessing(text):
     list_of_words = re.split(r"\s+", text)
     clear_words = []
@@ -24,4 +14,15 @@ def preprocessing(text):
         r = re.compile(r"[^a-zA-Z0-9]+")
         word = r.sub("", word)
         clear_words.append(word)
+    clear_words = list(filter(None, clear_words))
     return clear_words
+
+input_path = 'dataset/spam_ham_dataset.csv'
+
+dataset = pd.read_csv(input_path)
+texts = dataset['text']
+y = dataset['label_num']
+
+tokens_df = [preprocessing(text) for text in texts]
+
+print(tokens_df[0])
