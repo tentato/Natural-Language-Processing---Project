8da48f7a99ecbc0e30dfcb5da657835de7b082d9
tentato <alepaw99@gmail.com>
2023-05-08 23:04:10 +0200

    preprocessing function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from nltk.corpus import stopwords
+import re
 
 input_path = 'dataset/spam_ham_dataset.csv'
 
@@ -12,4 +13,15 @@ dataset = pd.read_csv(input_path)
 X = dataset['text']
 y = dataset['label_num']
 
-print(y)
+words_df = [preprocessing(text) for text in X]
+
+print(words_df[0])
+
+def preprocessing(text):
+    list_of_words = re.split(r"\s+", text)
+    clear_words = []
+    for word in list_of_words:
+        r = re.compile(r"[^a-zA-Z0-9]+")
+        word = r.sub("", word)
+        clear_words.append(word)
+    return clear_words
