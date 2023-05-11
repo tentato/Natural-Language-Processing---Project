44a3f6278314c8d5f2602e749c4215fc0b0b6650
tentato <alepaw99@gmail.com>
2023-05-11 20:55:13 +0200

    prepared for more datasets

--- classification.py
+++ classification.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+import os
 from sklearn import metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -16,28 +17,27 @@ clfs = {
     "ABC": AdaBoostClassifier(),
     "GBC": GradientBoostingClassifier()
 }
- 
-with open('result.json', 'r') as openfile:
-    json_object = json.load(openfile)
 
-labels = json_object['label']
-contents = json_object['content']
-contents_joined = [" ".join(s) for s in contents]
+for input in os.listdir("input"):
+    print(input)
+    exit()
+    with open('result.json', 'r') as openfile:
+        json_object = json.load(openfile)
 
-X_train, X_test, y_train, y_test = train_test_split(contents_joined, labels, test_size=0.33, random_state=10)
+    labels = json_object['label']
+    contents = json_object['content']
+    contents_joined = [" ".join(s) for s in contents]
 
-# TF - IDF
-tf_idf = TfidfVectorizer()
-X_train_tf = tf_idf.fit_transform(X_train)
-print(X_train_tf[0].shape)
-X_train_tf = tf_idf.transform(X_train)
+    X_train, X_test, y_train, y_test = train_test_split(contents_joined, labels, test_size=0.33, random_state=10)
 
-X_test_tf = tf_idf.transform(X_test)
-print(X_test_tf[0].shape)
-
+    # TF - IDF
+    tf_idf = TfidfVectorizer()
+    X_train_tf = tf_idf.fit_transform(X_train)
+    X_train_tf = tf_idf.transform(X_train)
+    X_test_tf = tf_idf.transform(X_test)
 
 
-for clf in clfs:
-    clf = clf.fit(X_train_tf, y_train)
-    y_pred = clf.predict(X_test_tf)
-    print(metrics.classification_report(y_test, y_pred, target_names=['Positive', 'Negative']))
+    for clf in clfs:
+        clf = clf.fit(X_train_tf, y_train)
+        y_pred = clf.predict(X_test_tf)
+        print(metrics.classification_report(y_test, y_pred, target_names=['Positive', 'Negative']))
